803f008f6197e013622d29f8a871ae7ea2b46a84
Brad Spurrell <63411329+bspur11@users.noreply.github.com>
2021-01-01 15:49:34 -0500

    Q Array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,28 @@
 // TODO: Create an array with five question objects
+var questions = [
+
+  {
+    prompt: "What type of brachets are used for an array?\n(a) ()\n(b) {}\n(c) []\n(d) All of the above",
+    answer: "c"
+  },
+  {
+    prompt: "What command is used to add a Child node to a Parent node in Javascript?\n(a) querySelector\n(b) getItemById\n(c) textContent\n(d) appendChild",
+    Answer: "d"
+  },
+  {
+    prompt: "What command in javascript saves strings in local storage?\n(a) localStorage.getItem\n(b) localStorage.appendChild\n(c) localStorage.setItem\n(d) None of the above",
+    answer: "c"
+  },
+  {
+    prompt: "Commonly used data types do not include:\n(a) Numbers\n(b) Strings\n(c) booleans\n(d) alerts",
+    answer: "c",
+  },
+  {
+    prompt: "Arrays in Javascript can be used to store______.\n(a)Numbers\n(b) Strings\n(c) Other Arrays\n(d) All of the above",
+    anxwer: "d",
+  },
+
+]
 
 // TODO: Create a variable to keep track of the score
 
