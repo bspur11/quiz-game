a2bff9eabaf67c87d794c49885055fd123eebc26
Brad Spurrell <63411329+bspur11@users.noreply.github.com>
2021-01-02 00:02:04 -0500

    start button

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,8 @@ startButton.addEventListener('click', startGame);
 function startGame() {
   console.log('Started');
   startButton.classList.add('hide');
+  questionContainerElement.classList.remove('hide');
+  setNextQuestion();
 }
 
 function setNextQuestion() {
